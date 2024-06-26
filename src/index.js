4450850cacb80c32305016961c980809d65888fe
Vineet Likhar <vineetlikhar1998@gmail.com>
2024-06-26 20:35:53 +0000

    add express listen(on) and throw error

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import mongoose from "mongoose";
 import { DB_NAME } from "./constants";
+import express from "express";
+
+const app = express();
 
 (async () => {
   try {
     await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+    app.on("error", (error) => {
+      console.log("ERROR: ", error);
+      throw error;
+    });
   } catch (error) {
     console.error("ERROR: ", error);
     throw error;
