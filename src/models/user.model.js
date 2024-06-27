b69e65fe127f638bbf7a5669500c5d6c3e785f15
Vineet Likhar <vineetlikhar1998@gmail.com>
2024-06-27 02:10:37 +0000

    add video model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -32,7 +32,7 @@ const userSchema = new Schema(
     },
     watchHistory: [
       {
-        type: mongoose.Schema.Types.ObjectId,
+        type: Schema.Types.ObjectId,
         ref: "Video",
       },
     ],
---
+++ src/models/video.model.js
@@ -0,0 +1,41 @@
+import mongoose, { Schema } from "mongoose";
+
+const videoSchema = new Schema(
+  {
+    videoFile: {
+      type: String,
+      required: true,
+    },
+    thumbnail: {
+      type: String,
+      requried: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    duration: {
+      type: Number, // after uploading video on cloudinary it will return file info, in which duration is one of the field
+      required: true,
+    },
+    views: {
+      type: Number,
+      default: 0,
+    },
+    isPublished: {
+      type: Boolean,
+      default: true,
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+export const Video = mongoose.model("Video", videoSchema);
