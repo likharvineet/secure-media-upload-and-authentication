326646d74e2ba214918cb72955ca4c51937e9cd8
Vineet Likhar <vineetlikhar1998@gmail.com>
2024-06-29 23:08:21 +0000

    add: return response

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -53,6 +53,9 @@ const registerUser = asyncHandler(async (req, res) => {
     throw new ApiError(500, "Something went wrong while registering the user");
   }
   // return res
+  return res
+    .status(201)
+    .json(new ApiResponse(200, createdUser, "User registered Successfully"));
 });
 
 export { registerUser };
