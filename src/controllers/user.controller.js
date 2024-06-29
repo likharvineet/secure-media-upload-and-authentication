dd5e45533e0a04ec81fbe936d621828b92b85704
Vineet Likhar <vineetlikhar1998@gmail.com>
2024-06-29 23:49:09 +0000

    fix: if cover image is not passed

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -25,7 +25,15 @@ const registerUser = asyncHandler(async (req, res) => {
   // check for images, check if avatar is passed
   // avatar and coverImage jaise humne route me name me define kiya hai same vaisa chahiye
   const avatarLocalPath = req.files?.avatar[0]?.path;
-  const coverImageLocalPath = req.files?.coverImage[0]?.path;
+  // const coverImageLocalPath = req.files?.coverImage[0]?.path
+  let coverImageLocalPath;
+  if (
+    req.files &&
+    Array.isArray(req.files.coverImage) &&
+    req.files.coverImage.length > 0
+  ) {
+    coverImageLocalPath = req.files.coverImage[0].path;
+  }
   if (!avatarLocalPath) {
     throw new ApiError(400, "Avatar file is required");
   }
