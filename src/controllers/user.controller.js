48738c8aed82b4abd9017b0f709258ebe3215685
Vineet Likhar <vineetlikhar1998@gmail.com>
2024-06-29 22:00:17 +0000

    add: step for user register

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,16 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const registerUser = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    message: "ok",
-  });
+  // Steps:
+  // get user details from frontend
+  // validation - not empty
+  // check if user already exists: using username, email
+  // check for images, check if avatar is passed
+  // upload them to cloudinary, avatar
+  // create user object - create entry in db
+  // remove password and refresh token field form response
+  // check for user creation
+  // return res
 });
 
 export { registerUser };
