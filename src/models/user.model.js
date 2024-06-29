d86de4699e7e2d6b16ef2b1fd915a57af5f06c09
Vineet Likhar <vineetlikhar1998@gmail.com>
2024-06-29 22:09:37 +0000

    update: fullname to fullName

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -19,7 +19,7 @@ const userSchema = new Schema(
       lowercase: true,
       trim: true,
     },
-    fullname: {
+    fullName: {
       type: String,
       required: true,
       index: true,
@@ -64,7 +64,7 @@ userSchema.methods.generateAccessToken = function () {
       _id: this._id,
       email: this.email,
       username: this.username,
-      fullname: this.fullname,
+      fullName: this.fullName,
     },
 
     process.env.ACCESS_TOKEN_SECRET,
