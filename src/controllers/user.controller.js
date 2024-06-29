f74df382d01a5a74c15ce9776f833454587ffdb7
Vineet Likhar <vineetlikhar1998@gmail.com>
2024-06-29 22:10:12 +0000

    add: check if data is coming

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,6 +3,8 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 const registerUser = asyncHandler(async (req, res) => {
   // Steps:
   // get user details from frontend
+  const { fullName, email, username, password } = req.body;
+  console.table([fullName, email, username, password]);
   // validation - not empty
   // check if user already exists: using username, email
   // check for images, check if avatar is passed
